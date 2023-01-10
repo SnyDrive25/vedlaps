41a4ca2607ec78717f71d52dad3c6f0cf9ab8436
SnyDrive25 <sunilgouvi@gmail.com>
2023-01-10 23:51:05 +0100

    spotify

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Home.css';
 import logo from '../../images/logo.png';
+import Spotify from 'react-spotify-embed';
 
 function Home() {
+
   return (
     <div className="Home">
       <img src={logo} alt="logo"></img>
       <h1>VedLaps</h1>
       <h2>Le podcast de Vinci Eco Drive</h2>
+      <div className='spot'>
+        <Spotify wide link="https://open.spotify.com/episode/26h7TIS5bcpIjvHnCahWLG" className='spot-element' />
+      </div>
     </div>
   );
 }
