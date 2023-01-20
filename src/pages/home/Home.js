7f5281dbfb00113fb9b0c404b22db95b4615c3cd
SnyDrive25 <sunilgouvi@gmail.com>
2023-01-20 23:36:01 +0100

    first part, pc version

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-//import { useState } from 'react';
+import phone from '../../images/iphone.png';
 
 function Home() {
 
@@ -8,9 +8,27 @@ function Home() {
 
   return (
     <div className="Home">
-      <h1>VedLaps</h1>
-      <h2>Le podcast de Vinci Eco Drive</h2>
+      <div className='master'>
+        <div className='left'>
+          <h1>VedLaps</h1>
+          <h2>Le podcast</h2>
+          <p>
+            Ce podcast de Vinci Eco Drive
+            vous propose de découvrir des
+            personnalité en lien avec nos
+            projet et l'association mais
+            également des personnalités
+            différentes apportant une
+            nouvelle vision sur notre monde.
+          </p>
+          <button>Écouter le dernier épisode</button>
+        </div>
+        <div className='right'>
+          <img src={phone} alt='vedlaps_iphone'></img>
+        </div>
+      </div>
       <div className='spot'>
+        <h1>Le concept</h1>
         <iframe title='lap1' className='spot-elmt' src="https://open.spotify.com/embed/show/3KRDSrplsy5jYVssZHm2uM?utm_source=generator" allowFullScreen="" loading="lazy"></iframe>
       </div>
     </div>
